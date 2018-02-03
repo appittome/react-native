d7b68450935221568a77d662dfeca86370b5e84f
Evan J Brunner <ej3@appitto.me>
2018-02-02 18:13:24 -0800

    update websocket_test_server to set cookie in handshake
    Signed-off-by: Evan J Brunner <ej3@appitto.me>

--- RNTester/js/examples/WebSocket/websocket_test_server.js
+++ RNTester/js/examples/WebSocket/websocket_test_server.js
@@ -28,8 +28,12 @@ ArrayBuffer instead of a string.
 
 const respondWithBinary = process.argv.indexOf('--binary') !== -1;
 const server = new WebSocket.Server({port: 5555});
-server.on('connection', ws => {
-  ws.on('message', message => {
+var count = 0;
+server.on('headers', (headers, req) => {
+  headers.push('Set-Cookie: wstest=WS-' + count++ + ';');
+});
+server.on('connection', (ws) => {
+  ws.on('message', (message) => {
     console.log('Received message:', message);
     if (respondWithBinary) {
       message = Buffer.from(message);
@@ -41,6 +45,7 @@ server.on('connection', ws => {
     ws.send(message);
   });
 
+  console.log('Cookie:', ws.upgradeReq.headers.cookie);
   console.log('Incoming connection!');
   ws.send('Why hello there!');
 });
